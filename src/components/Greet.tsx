e3379088c9a91ed33dc103684b38ab020d6bdeb6
Corinto <salazar72390@gmail.com>
2023-09-06 13:03:48 +0800

    advance props

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,10 +1,16 @@
 type GreetProps = {
   name: string;
+  messageCount: number;
+  isLoggedIn: boolean;
 };
 export const Greet = (props: GreetProps) => {
   return (
     <div>
-      <h1>Hello World! {props.name}</h1>
+      <h2>
+        {props.isLoggedIn
+          ? ` Welcome &{props.name}! You have &{props.messageCount} unread messages`
+          : "Welcome Guest"}
+      </h2>
     </div>
   );
 };
