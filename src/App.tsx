d7103c5c59b71780fa51376f2f1c78047dfe0a54
Corinto <salazar72390@gmail.com>
2023-09-06 15:29:01 +0800

    useContext Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,13 @@
+import { ThemeContextProvider } from "./components/context/ThemeContext";
+import { Box } from "./components/context/Box";
 import "./App.css";
-import { Container } from "./Container";
-import { Button } from "./components/Button";
-import { Greet } from "./components/Greet";
-import { Heading } from "./components/Heading";
-import { Input } from "./components/Input";
-import { Oscar } from "./components/Oscar";
-import { Person } from "./components/Person";
-import { PersonList } from "./components/PersonList";
-import { Status } from "./components/Status";
 
 function App() {
-  const personName = {
-    first: "Bruce",
-    last: "Wayne",
-  };
-
-  const nameList = [
-    {
-      first: "Bruce",
-      last: "Wayne",
-    },
-    {
-      first: "Clark",
-      last: "Kent",
-    },
-    {
-      first: "Princess",
-      last: "Diana",
-    },
-  ];
-
   return (
     <div className="App">
-      <Greet name="Corinto" isLoggedIn={false} />
-      <Person name={personName} />
-      <PersonList names={nameList} />
-      <Status status="success" />
-      <Heading>Placeholder</Heading>\
-      <Oscar>
-        <Heading>Oscar goes to Leonardo Dicaprio</Heading>
-      </Oscar>
-      <Button
-        handleClick={(event, id) => {
-          console.log("Button clicked", event, id);
-        }}
-      />
-      <Input value="" handleChange={(event) => console.log(event)} />
-      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
     </div>
   );
 }
