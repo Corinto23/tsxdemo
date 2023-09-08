841b062693db9fa64219279599cd9196c6675a32
Corinto <salazar72390@gmail.com>
2023-09-08 17:53:46 +0800

    Template Literals and Exclude

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
-import { RandomNumber } from "./components/restriction/RandomNumber";
+import { Toast } from "./components/templateliterals/Toast";
 function App() {
   return (
     <div className="App">
-      <RandomNumber value={10} isPositive />
+      <Toast position="center-top" />
     </div>
   );
 }
